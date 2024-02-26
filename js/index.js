666b306871826a0630d23d025c1ce345766750f8
ZellKirov92 <gwen.dragonrouge@gmail.com>
2024-02-27 00:17:56 +0100

    modif index.js for dark and light modif modif style.css selector and style modif index.html class for dark and light mode

--- js/index.js
+++ js/index.js
@@ -1,34 +1,35 @@
 const switchButton = document.getElementsByClassName("switch_button")[0];
 switchButton.addEventListener("change", (ev) => {
     const body = document.getElementsByTagName("body")[0];
-    const article = document.getElementsByClassName("light_dark")[0];
-    const section = document.querySelectorAll("section");
+    const articleContent = document.getElementsByClassName("light_dark")[0];
+    const section = document.getElementsByTagName("section");
     const textBold = document.getElementsByClassName("textBold");
     const textElements = document.getElementsByClassName("text");
 
     if (switchButton.checked) {
-        body.style.backgroundColor = "hsl(0, 0%, 100%)";
-        article.style.backgroundColor = "hsl(225, 100%, 98%)";
+        body.classList.add("lightTheme");
+        articleContent.classList.add("articleLightTheme");
         for(const elemsections of section) {
-        elemsections.style.backgroundColor = "hsl(227, 47%, 96%)";
+        elemsections.classList.add("elemSectionLight");
         }
         for(const elembold of textBold) {
-        elembold.style.color = "hsl(230, 17%, 14%)";
+        elembold.classList.add("elemBoldLight");
         }
         for (const elem of textElements) {
-            elem.style.color = "hsl(228, 12%, 44%)";
+            elem.classList.add("elemTextLight");
         }
-    } else {
-    body.style.backgroundColor = "hsl(230, 17%, 14%)";
-    article.style.backgroundImage = "linear-gradient(to bottom hsl(232, 19%, 15%) 20%, hsl(230, 17%, 14%))";
-    for (const elemsections of section) {
-        elemsections.style.backgroundColor = "hsl(228, 28%, 20%)";
-    }
-    for (const elembold of textBold) {
-        elembold.style.color = "hsl(0, 0%, 100%)";
-    }
-    for (const elem of textElements) {
-        elem.style.color = "hsl(228, 34%, 66%)";
+    } 
+    else {
+        body.classList.remove("lightTheme");
+        articleContent.classList.remove("articleLightTheme");
+        for(const elemsections of section) {
+        elemsections.classList.remove("elemSectionLight");
         }
+        for(const elembold of textBold) {
+        elembold.classList.remove("elemBoldLight");
+        }
+        for (const elem of textElements) {
+            elem.classList.remove("elemTextLight");
+        }    
     }
 });
